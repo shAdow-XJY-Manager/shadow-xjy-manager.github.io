dfb5f1c72f45066e911479d95138748204662b68
ShadowPlusing <1290232854@qq.com>
2025-09-29 22:15:30 +0800

    Feat: clean the master, (current content transferred to old_master)

--- android/app/src/main/kotlin/com/example/github_blog/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.github_blog
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/innerAssets/bookAsset/bookData.dart
+++
@@ -1,9 +0,0 @@
-class BookData {
-  static Map<String, Map<String, String>> bookData = {
-    "bookName 1": {
-      "status": "writing",
-      "type": "type 1",
-      "description": "description 1",
-    }
-  };
-}
